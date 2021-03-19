b4c6b729005b39810992a6a822bd3799914c5980
Jmark-Abi <jmaking12345@gmail.com>
2021-03-19 21:58:16 +0800

    withdraw method done

---
+++ SimpleProg/src/java/com/BankingSystem/BankingSystem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BankingSystem;
+
+/**
+ *
+ * @author jm
+ */
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class BankingSystem {
+    
+    private int money;
+    private int balance;
+    private int loanMoney;
+    private int durationPayment;
+    
+     @WebMethod
+     public void setWithdraw(int money){
+         this.money = money;
+     }
+     
+     @WebMethod
+     public int Withdraw(){
+         if (balance == 0 || balance < money){
+             System.out.println("Not enough balance to withdraw");
+         } else {
+              balance -= money;
+         }
+         return balance;
+     }
+}
